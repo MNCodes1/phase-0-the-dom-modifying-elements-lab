8819e09422dd27110ce1f6263264cc9595765632
Maryanne Kimani <maranne.kim@gmail.com>
2025-09-29 09:00:01 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+// Remove the <main> element with id 'main'
+document.querySelector('main#main').remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Set its id to 'victory'
+newHeader.id = 'victory';
+
+// Set its text content to "YOUR-NAME is the champion"
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+// Append it to the document body
+document.body.appendChild(newHeader);
